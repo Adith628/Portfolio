d5821196a7c4c7207e357c741bc512a9a11234c1
Adithyan T <adithsreedhar628@gmail.com>
2023-11-05 21:35:37 +0530

    updated resume

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,7 +50,7 @@ export default function Home() {
                  className="cursor-pointer" /> 
               </li>
               <li >
-                <a target="_blank" rel="noreferrer"  href="https://drive.google.com/file/d/13YibjJEpJzMSS1AASGz7T5qeCI8WjqUa/view?usp=sharing" 
+                <a target="_blank" rel="noreferrer"  href="https://drive.google.com/drive/folders/1xI29OKW03lksQ-O8Fi-7LGyV4E8KQmrt?usp=sharing" 
                 className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' 
                 >
                   Resume
